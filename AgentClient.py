17c78e7b624e87e95702b2e34e8e49d29ebb6845
AzzeriArtess <ahin.artess2698@gmail.com>
2021-05-13 20:29:37 +0200

    Album class refactorized

--- AgentClient.py
+++ AgentClient.py
@@ -4,19 +4,19 @@ from tkinter import ttk, StringVar, W
 from ttkthemes import ThemedTk
 from Album import Album
 
-albumsArr = []
-atmospheres = ["Smutna", "Lekka"]
-languages = ["polski"]
-ocassions = ["Relaks", "Praca"]
-album = Album("Ballady", "Kat", "1989-10-10", 50, "Kompakt", 1, 1, atmospheres, languages, ocassions, 25.5)
-albumsArr.append(album)
-album = Album("Death By Rock And Roll", "The Pretty Reckless", "1989-10-10", 50, "Kompakt", 1, 1, atmospheres,
-              languages, ocassions, 20.0)
-albumsArr.append(album)
-album = Album("Infestissumam", "Ghost", "1989-10-10", 50, "Kompakt", 1, 1, atmospheres, languages, ocassions, 40)
-albumsArr.append(album)
-album = Album("Róże", "KAT", "1989-10-10", 50, "Kompakt", 1, 1, atmospheres, languages, ocassions, 50)
-albumsArr.append(album)
+# albumsArr = []
+# atmospheres = ["Smutna", "Lekka"]
+# languages = ["polski"]
+# ocassions = ["Relaks", "Praca"]
+# album = Album("Ballady", "Kat", "1989-10-10", 50, "Kompakt", 1, atmospheres, languages, ocassions, 25.5)
+# albumsArr.append(album)
+# album = Album("Death By Rock And Roll", "The Pretty Reckless", "1989-10-10", 50, "Kompakt", 1, atmospheres,
+#               languages, ocassions, 20.0)
+# albumsArr.append(album)
+# album = Album("Infestissumam", "Ghost", "1989-10-10", 50, "Kompakt", 1, atmospheres, languages, ocassions, 40)
+# albumsArr.append(album)
+# album = Album("Róże", "KAT", "1989-10-10", 50, "Kompakt", 1, atmospheres, languages, ocassions, 50)
+# albumsArr.append(album)
 
 
 class AgentClient:
@@ -38,8 +38,8 @@ class AgentClient:
         print(language.array, language.priority, sep=" ")
         print(maxprice)
 
-        rand = random.randrange(0, len(albumsArr), 1)
-        while float(albumsArr[rand].price) > float(maxprice):
-            rand = random.randrange(0, len(albumsArr), 1)
-
-        return albumsArr[rand]
+        # rand = random.randrange(0, len(albumsArr), 1)
+        # while float(albumsArr[rand].price) > float(maxprice):
+        #     rand = random.randrange(0, len(albumsArr), 1)
+        #
+        # return albumsArr[rand]
--- Album.py
+++ Album.py
@@ -1,15 +1,20 @@
 class Album:
-    def __init__(self, name, artist, date, length, albumformat, ispolish, isvocal, atmospheres, languages, occasions,
-                 price):
+    def __init__(self, name, artist, date, length, albumformat, ispolish, atmospheres, languages, occasions,
+                 price, yearsmatch, atmospheresmatch, lengthmatch, languagesmatch, nationmatch, occassionsmatch):
         self.name = name
         self.artist = artist
         self.date = date
         self.length = length
         self.albumformat = albumformat
         self.ispolish = ispolish
-        self.isvocal = isvocal
         self.atmospheres = atmospheres
         self.languages = languages
         self.occasions = occasions
         self.price = price
 
+        self.years = yearsmatch
+        self.atmospheresmatch = atmospheresmatch
+        self.lengthmatch = lengthmatch
+        self.languagesmatch = languagesmatch
+        self.nationmatch = nationmatch
+        self.occassionsmatch = occassionsmatch
