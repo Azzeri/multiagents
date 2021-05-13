b4ed192f8978627cd706aaa84f28ceb380195d67
AzzeriArtess <ahin.artess2698@gmail.com>
2021-05-13 21:30:50 +0200

    album class refactorized v2

--- Album.py
+++ Album.py
@@ -1,20 +1,51 @@
+import datetime
+
+
 class Album:
-    def __init__(self, name, artist, date, length, albumformat, ispolish, atmospheres, languages, occasions,
-                 price, yearsmatch, atmospheresmatch, lengthmatch, languagesmatch, nationmatch, occassionsmatch):
+    def __init__(self, name, artist, date, length, albumformat, price, atmospheresmatch, languagesmatch, nationmatch,
+                 occassionsmatch):
         self.name = name
         self.artist = artist
         self.date = date
         self.length = length
         self.albumformat = albumformat
-        self.ispolish = ispolish
-        self.atmospheres = atmospheres
-        self.languages = languages
-        self.occasions = occasions
         self.price = price
 
-        self.years = yearsmatch
         self.atmospheresmatch = atmospheresmatch
-        self.lengthmatch = lengthmatch
         self.languagesmatch = languagesmatch
         self.nationmatch = nationmatch
         self.occassionsmatch = occassionsmatch
+
+        if length <= 15:
+            self.lengthmatch = [3, 0, 0]
+        elif 15 < length <= 30:
+            self.lengthmatch = [2, 1, 0]
+        elif 30 < length <= 45:
+            self.lengthmatch = [1, 2, 1]
+        elif 45 < length <= 60:
+            self.lengthmatch = [0, 3, 2]
+        elif 60 < length <= 75:
+            self.lengthmatch = [0, 2, 3]
+        elif length > 75:
+            self.lengthmatch = [0, 1, 3]
+
+        nodays = ((datetime.datetime.now() - self.date).days / 365)
+        if nodays >= 51:
+            self.yearsmatch = [3, 0, 0]
+        elif 51 > nodays >= 31:
+            self.yearsmatch = [2, 1, 0]
+        elif 31 > nodays >= 21:
+            self.yearsmatch = [1, 2, 1]
+        elif 21 > nodays >= 11:
+            self.yearsmatch = [0, 3, 2]
+        elif 11 > nodays >= 1:
+            self.yearsmatch = [0, 2, 3]
+        elif nodays < 1:
+            self.yearsmatch = [0, 1, 3]
+
+    # self.ispolish = ispolish
+    # self.atmospheres = atmospheres
+    # self.languages = languages
+    # self.occasions = occasions
+    # self.yearsmatch = yearsmatch#calc
+    # self.lengthmatch = lengthmatch#calc
--- Magasine.py
+++ Magasine.py
@@ -4,4 +4,22 @@ from Album import Album
 class Magasine:
     def __init__(self, name):
         self.name = name
-        self.albums = []
+        # self.albums = [Album()]
+       # y = datetime.datetime(2020, 5, 13)
+
+# The Pretty Reckless - Death by Rock and Roll	0	0	3	1	2	1	3	1	3	1	1	3	1	3	3	3	0	3	0	0	3	2	3	0	1	1
+# Lacuna Coil - Unleashed Memories	0	1	3	3	0	2	1	3	1	1	1	3	1	3	0	3	0	3	1	0	3	0	0	1	2	3
+# Kat - Ballady	0	3	1	2	1	2	2	3	1	2	1	3	1	3	0	3	3	0	0	3	0	1	1	1	1	3
+# Ewelina Lisowska - Ponad Wszystko	0	0	3	3	1	3	0	3	1	2	1	3	1	0	0	3	3	0	0	3	0	1	1	2	3	3
+# Linkin Park - Hybrid Theory	0	2	2	2	2	1	2	1	2	1	1	2	1	3	3	3	0	3	0	0	3	3	2	1	1	1
+# Ghost - Infestissumam	0	0	3	2	1	2	2	1	2	3	1	2	1	3	3	3	0	3	1	0	3	2	1	2	2	2
+# Mgła - Age of Excuse	0	0	3	2	0	0	3	1	3	1	2	2	0	3	3	3	0	3	0	3	0	1	2	0	0	1
+# Hunter - Arachne	0	0	3	2	1	1	2	1	1	2	1	2	1	3	0	3	3	0	1	3	0	1	1	0	1	2
+# Mgła - Exercises in Futility	0	0	3	2	0	0	3	1	3	1	2	2	0	3	0	3	0	3	0	3	0	1	2	0	0	1
+# Evanescence - Fallen	0	2	2	3	0	1	2	1	1	2	1	2	1	0	0	3	0	3	0	0	3	2	2	0	1	2
+# Hunter - Królestwo	0	0	3	2	1	1	2	1	2	2	2	1	0	3	3	3	3	0	0	3	0	2	1	1	1	1
+# Ghost - Meliora	0	0	3	1	2	1	2	1	1	3	1	2	0	3	3	3	0	3	1	0	3	2	1	1	1	2
+# Kat - Oddech Wymarłych Światów	2	2	0	2	1	1	3	1	3	2	1	2	0	3	3	3	3	1	0	3	0	3	3	0	0	1
+# Me and that Man - Songs of Love and Death	0	0	3	2	0	1	1	1	1	2	1	2	1	3	3	3	0	3	0	3	3	1	1	2	1	3
+# Kwiat Jabłoni - Niemożliwe	0	0	3	2	2	3	0	2	0	1	1	2	1	3	3	3	3	0	0	3	0	1	0	2	2	3
+# Iron Maiden - Iron Maiden	3	1	0	1	2	0	2	1	3	1	2	1	0	3	0	3	0	3	0	0	3	3	3	1	1	1
